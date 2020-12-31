1bb3a0552b9cbc578d63896a947349a28adf67de
oliverloops <oliveralr@protonmail.com>
2020-12-31 00:22:24 -0600

    new layout updates

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,25 @@
 import Link from "next/link";
+import { motion } from "framer-motion";
 import { HiMoon } from "react-icons/hi";
 import { HiSun } from "react-icons/hi";
 
 export default function Header() {
   return (
-    <nav className="flex justify-around flex-row fixed sticky sticky-nav top-0 h-18 py-8 bg-white dark:bg-black bg-opacity-60">
+    <motion.nav
+      initial="pageInitial"
+      animate="pageAnimate"
+      variants={{
+        pageInitial: {
+          opacity: 0,
+          translateY: -200,
+        },
+        pageAnimate: {
+          opacity: 1,
+          translateY: 0,
+        },
+      }}
+      className="flex justify-around flex-row fixed sticky sticky-nav top-0 h-18 py-16 bg-white dark:bg-black bg-opacity-60"
+    >
       <div>
         <button className="flex justify-center items-center rounded-md bg-gray-200 h-11 w-11 ">
           <HiMoon className="icon" />
@@ -32,6 +47,6 @@ export default function Header() {
           </Link>
         </div>
       </div>
-    </nav>
+    </motion.nav>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
+import { motion } from "framer-motion";
+
 export default function Home() {
   return (
-    <>
+    <motion.div
+      initial="pageInitial"
+      animate="pageAnimate"
+      variants={{
+        pageInitial: {
+          opacity: 0,
+          translateX: -800,
+        },
+        pageAnimate: {
+          opacity: 1,
+          translateX: 0,
+        },
+      }}
+    >
       <h1 className="font-bold text-3xl">Hi, I'm Oliver Lopez</h1>
       <h3 className="text-gray-500">
         I'm a software developer, rookie barista, chef amateur, writer and
         poetry lover.
       </h3>
-    </>
+    </motion.div>
   );
 }
