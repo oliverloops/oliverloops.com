a3ab591ad3cd8a1391159e54111b048c034c59e2
oliverloops <oliveralr@protonmail.com>
2021-01-05 11:01:39 -0600

    adding blog filter

--- components/Search.js
+++ components/Search.js
@@ -2,14 +2,18 @@ import { MdSearch } from "react-icons/md";
 
 const Search = (props) => {
   return (
-    <div className="search-container">
+    <div className="search-container mb-4 border-gray-300 bg-white dark:bg-gray-700">
       {/* <input
         type="text"
         value={props.inputValue}
         onChange={props.cardFilter}
         placeholder="Buscar"
       /> */}
-      <input type="text" placeholder="Search" />
+      <input
+        className="bg-white dark:bg-gray-700 dark:text-white"
+        type="text"
+        placeholder="Search"
+      />
       <MdSearch className="search" size={30} />
     </div>
   );
--- pages/blog.js
+++ pages/blog.js
@@ -5,8 +5,12 @@ import { posts } from "../getAllPosts";
 
 export default function Blog() {
   return (
-    <div className="flex flex-col ">
+    <div className="flex flex-col">
       <h1 className="text-3xl font-extrabold dark:text-white">Blog</h1>
+      <p className="mt-4 mb-4 dark:text-white">
+        I want to start writing and sharing all my experiences, projects and
+        other cool stuff.
+      </p>
       <Search />
       {posts
         .slice(0)
