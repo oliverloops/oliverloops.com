7f92f1ff17ef81ff10dfd759bc9fd5273b31debd
oliverloops <oliveralr@protonmail.com>
2021-01-06 12:31:57 -0600

    updating layout

--- components/AuthorHeadPost.js
+++ components/AuthorHeadPost.js
@@ -10,10 +10,17 @@ export default function AuthorHeadPost({ meta, link }) {
       <div className="text-xl font-semibold   dark:text-white">
         {meta.description}
       </div>
-      <div className="flex justify-between row">
-        {/* <Image /> */}
-        <p>{meta.author}</p>
-        <div className="text-gray-400">{meta.date}</div>
+      <div className="flex justify-between space-x-2 row">
+        <Image
+          className="rounded-full"
+          src={"/me_cross.jpg"}
+          alt={"Oliver Lopez"}
+          width={25}
+          height={25}
+        />
+        <p className="text-gray-700 dark:text-gray-300">{meta.author}</p>
+        <span>-</span>
+        <div className="text-gray-700 dark:text-gray-300">{meta.date}</div>
       </div>
       <div className="flex justify-between row">
         <div className="text-gray-400">
