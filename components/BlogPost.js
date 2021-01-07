24ea6a11b65b36a16880f05d6131ff8ddc0514d0
oliverloops <oliveralr@protonmail.com>
2021-01-06 20:35:27 -0600

    new layout and responsive updates

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -2,7 +2,7 @@ import AuthorHeadPost from "./AuthorHeadPost";
 
 export default function BlogPost({ children, meta }) {
   return (
-    <article>
+    <article className="flex flex-col justify-center max-w-2xl mx-auto mb-4 w-full">
       <AuthorHeadPost meta={meta} isBlogPost />
       {children}
     </article>
