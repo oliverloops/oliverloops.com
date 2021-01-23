1fdb45933d9887f95b4e874a6c9dae7ea17a439a
oliverloops <oliveralr@protonmail.com>
2021-01-23 00:22:16 -0600

    New styling was added

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,28 +1,84 @@
 import Image from "next/image";
+import styled from "styled-components";
+
+//Custom styled components
+const Div = styled.div`
+  height: 320px;
+
+  @media (min-width: 300px) and (max-width: 768px) {
+    height: auto;
+  }
+`;
+
+const AppImage = styled(Image)`
+  border-radius: 10px;
+`;
 
 export default function Portfolio() {
   return (
     <div className="flex flex-col space-y-4 mx-auto">
       <a href="#">
-        <div className="bg-white dark:bg-gray-900 rounded-lg shadow hover:shadow-lg mx-0 lg:max-w-xl lg:mx-auto">
-          <div className="p-6">
+        <Div className="bg-white dark:bg-gray-900 rounded-lg shadow hover:shadow-lg mx-0 lg:max-w-xl lg:mx-auto">
+          <div className="p-4">
             <span className="flex">
-              <Image src={"/static/images/chief.png"} width={60} height={60} />
+              <AppImage
+                src={"/static/images/chief.png"}
+                width={58}
+                height={58}
+              />
+              <div className="flex flex-col ">
+                <p className="pt-2 px-4 text-lg font-semibold dark:text-white">
+                  Fitcloud.mx
+                </p>
+                <p className="px-4 font-semibold dark:text-white">
+                  Fitness sessions on demand
+                </p>
+              </div>
             </span>
           </div>
-        </div>
+        </Div>
       </a>
       <a href="#">
-        <div className="bg-white dark:bg-gray-900 rounded-lg shadow hover:shadow-lg mx-0 lg:max-w-xl lg:mx-auto">
-          <div className="p-6">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-              hendrerit elit quis ullamcorper vehicula. Cras sed scelerisque
-              odio. Etiam sem ipsum, lobortis a rutrum eget, suscipit in eros.
-              Nunc nunc eros, ultrices non diam in, rhoncus commodo leo.
-            </p>
+        <Div className="bg-white dark:bg-gray-900 rounded-lg shadow hover:shadow-lg mx-0 lg:max-w-xl lg:mx-auto">
+          <div className="p-4">
+            <span className="flex">
+              <AppImage
+                src={"/static/images/chief.png"}
+                width={58}
+                height={58}
+              />
+              <div className="flex flex-col ">
+                <p className="pt-2 px-4 text-lg font-semibold dark:text-white">
+                  Barrio Intercambia
+                </p>
+                <p className="px-4 font-semibold dark:text-white">
+                  Support against COVID-19
+                </p>
+              </div>
+            </span>
           </div>
-        </div>
+        </Div>
+      </a>
+      <a href="#">
+        <Div className="bg-white dark:bg-gray-900 rounded-lg shadow hover:shadow-lg mx-0 lg:max-w-xl lg:mx-auto">
+          <div className="p-4">
+            <span className="flex">
+              <AppImage
+                src={"/static/images/chief.png"}
+                width={58}
+                height={58}
+              />
+              <div className="flex flex-col ">
+                <p className="pt-2 px-4 text-lg font-semibold dark:text-white">
+                  Kila
+                </p>
+                <p className="px-4  font-semibold dark:text-white">
+                  Environmental Services
+                </p>
+              </div>
+            </span>
+          </div>
+        </Div>
       </a>
     </div>
   );
