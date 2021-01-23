10eb0490f6b0e639ae7f410b8fd8f6498c1f4b15
oliverloops <oliveralr@protonmail.com>
2021-01-22 23:40:45 -0600

    dependency tree up to date

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,25 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 import darkMode from "../components/Layout";
 
 class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    // Create an instance of ServerStyleSheet
+    const sheet = new ServerStyleSheet();
+    // Retrieve styles from components in the page
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    // Extract the styles as <style> tags
+    const styleTags = sheet.getStyleElement();
+    // Pass styleTags as a prop
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>{this.props.styleTags}</Head>
         <body className="bg-white dark:bg-black">
           <Main />
           <NextScript />
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,20 +1,19 @@
+import Image from "next/image";
+
 export default function Portfolio() {
   return (
-    <div className="flex flex-col space-y-4 sm:mx-auto lg:mx-8">
+    <div className="flex flex-col space-y-4 mx-auto">
       <a href="#">
-        <div className="bg-white dark:bg-gray-900 rounded-lg shadow hover:shadow-lg mx-0 lg:mx-12">
+        <div className="bg-white dark:bg-gray-900 rounded-lg shadow hover:shadow-lg mx-0 lg:max-w-xl lg:mx-auto">
           <div className="p-6">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-              hendrerit elit quis ullamcorper vehicula. Cras sed scelerisque
-              odio. Etiam sem ipsum, lobortis a rutrum eget, suscipit in eros.
-              Nunc nunc eros, ultrices non diam in, rhoncus commodo leo.
-            </p>
+            <span className="flex">
+              <Image src={"/static/images/chief.png"} width={60} height={60} />
+            </span>
           </div>
         </div>
       </a>
       <a href="#">
-        <div className="bg-white dark:bg-gray-900 rounded-lg shadow hover:shadow-lg mx-0 lg:mx-12">
+        <div className="bg-white dark:bg-gray-900 rounded-lg shadow hover:shadow-lg mx-0 lg:max-w-xl lg:mx-auto">
           <div className="p-6">
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
