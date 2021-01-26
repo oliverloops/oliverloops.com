760524d930238a0f028dec58e8e34cc6a904beec
oliverloops <oliveralr@protonmail.com>
2021-01-26 17:24:15 -0600

    Adding tech stack images

--- pages/about.js
+++ pages/about.js
@@ -12,16 +12,18 @@ const Text = styled.p`
 const AppItem = ({ image, name, desc }) => (
   <>
     <a
-      className="flex rounded-lg sm:p-4 sm:hover:bg-gray-100 sm:dark:hover:bg-gray-900"
+      className="flex rounded-lg py-4 sm:p-4 sm:hover:bg-gray-100 sm:dark:hover:bg-gray-900"
       href="#"
     >
-      <Image
-        className="rounded-lg"
-        src={image}
-        width={58}
-        height={58}
-        alt={"VS Code icon"}
-      />
+      <div>
+        <Image
+          className="rounded-lg"
+          src={image}
+          width={58}
+          height={58}
+          alt={"VS Code icon"}
+        />
+      </div>
       <div className="flex flex-col justify-center flex-1 pl-5 space-y-2">
         <div className="flex flex-col">
           <p className="font-semibold text-black dark:text-white">{name}</p>
@@ -79,7 +81,7 @@ export default function About() {
         <div>
           <Image
             src={"/static/images/books/jamstack_book.png"}
-            width={155}
+            width={165}
             height={235}
             alt={"A book"}
           />
@@ -87,7 +89,7 @@ export default function About() {
         <div className="ml-4">
           <Image
             src={"/static/images/books/genoma_cover.jpeg"}
-            width={155}
+            width={165}
             height={235}
             alt={"A book"}
           />
@@ -100,7 +102,7 @@ export default function About() {
         <div>
           <Image
             src={"/static/images/books/opinionated_guide.png"}
-            width={155}
+            width={165}
             height={235}
             alt={"A book"}
           />
@@ -108,7 +110,7 @@ export default function About() {
         <div className="ml-4">
           <Image
             src={"/static/images/books/the_one_thing.jpeg"}
-            width={155}
+            width={165}
             height={235}
             alt={"A book"}
           />
@@ -116,7 +118,7 @@ export default function About() {
         <div className="ml-4">
           <Image
             src={"/static/images/books/hooked_cover.jpeg"}
-            width={155}
+            width={165}
             height={235}
             alt={"A book"}
           />
@@ -130,7 +132,7 @@ export default function About() {
           This is the technology stack I use the most on my daily work
         </Text>
       </div>
-      <div className="flex flex-col mt-2">
+      <div className="flex flex-col">
         <AppItem
           image={"/static/images/logos/vscode.png"}
           name={"Visual Studio Code"}
@@ -140,13 +142,37 @@ export default function About() {
           image={"/static/images/logos/figma_icon.png"}
           name={"Figma"}
           desc={
-            "My favorite design tool by far, I use it for UI mockups and design systems."
+            "My favorite design tool by far, I use it for architechting UI's and create design systems."
           }
         />
         <AppItem
           image={"/static/images/logos/notion_icon.png"}
           name={"Notion"}
           desc={"The super app where I manage my workflow."}
+        />
+        <AppItem
+          image={"/static/images/logos/nextjs_icon.jpeg"}
+          name={"Next.js"}
+          desc={"A powerful framework to build modern React websites."}
+        />
+        <AppItem
+          image={"/static/images/logos/vercel_icon.jpeg"}
+          name={"Vercel"}
+          desc={
+            "I started to deploy on Vercel and I need to say that the experience it's awesome. Just develop, ship and preview."
+          }
+        />
+        <AppItem
+          image={"/static/images/logos/telegram_x.png"}
+          name={"Telegram"}
+          desc={"My main and secure messaging client."}
+        />
+        <AppItem
+          image={"/static/images/logos/iterm2.jpg"}
+          name={"iTerm 2"}
+          desc={
+            "One of the fanciest terminal I've used, the first app you download when buy a mac."
+          }
         />
       </div>
     </motion.div>
