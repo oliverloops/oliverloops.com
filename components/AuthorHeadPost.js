b634682eb2512302f16301030e519ed5fcabc764
oliverloops <oliveralr@protonmail.com>
2021-01-30 12:51:27 -0600

    Adding new blog posts content

--- components/AuthorHeadPost.js
+++ components/AuthorHeadPost.js
@@ -3,32 +3,30 @@ import Image from "next/image";
 export default function AuthorHeadPost({ meta, link }) {
   return (
     <div className="flex justify-center flex-col space-y-2 mb-6">
-      <h1 className="mb-2 text-4xl font-extrabold  dark:text-white transition-all">
+      <h1 className="mb-2 text-4xl font-extrabold  dark:text-white">
         {meta.title}
       </h1>
       <div className="flex space-x-2 row">
         <Image
           className="rounded-full"
-          src={"/me_cross.jpg"}
+          src={"/static/images/me_cross.jpg"}
           alt={"Oliver Lopez"}
           width={25}
           height={25}
         />
         <p
           className="text-sm
-         text-gray-700 dark:text-gray-300 transition-all"
+         text-gray-700 dark:text-gray-300"
         >
           {meta.author}
         </p>
-        <span className="text-sm text-gray-700 dark:text-gray-300 transition-all">
-          -
-        </span>
-        <div className="text-sm text-gray-700 dark:text-gray-300 transition-all">
+        <span className="text-sm text-gray-700 dark:text-gray-300">-</span>
+        <div className="text-sm text-gray-700 dark:text-gray-300">
           {meta.date}
         </div>
       </div>
       <div className="flex justify-between row">
-        <div className="text-sm text-gray-400 dark:text-gray-500 transition-all">
+        <div className="text-sm text-gray-400 dark:text-gray-500">
           <span role="img" aria-label="book">
             📖
           </span>{" "}
--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>{this.props.styleTags}</Head>
+        <Head>
+          {this.props.styleTags}
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Amiri:ital@0;1&family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
         <body className="bg-white text-black dark:bg-black dark:text-white">
           <Main />
           <NextScript />
