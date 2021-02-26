514a8fb9edef2cebd4a5e681f867335d5d6199cd
oliverloops <oliveralr@protonmail.com>
2021-02-26 00:20:54 -0600

    Adding progress bar to posts

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -1,10 +1,22 @@
+import React from "react";
 import AuthorHeadPost from "./AuthorHeadPost";
 
 export default function BlogPost({ children, meta }) {
+  React.useEffect(() => {
+    let bar = document.querySelector("#progress");
+    window.addEventListener("scroll", () => {
+      let max = document.body.scrollHeight - innerHeight;
+      bar.style.width = `${(pageYOffset / max) * 100}%`;
+    });
+  }, []);
+
   return (
-    <article className="flex flex-col justify-center max-w-2xl mx-auto mb-4 w-full">
-      <AuthorHeadPost meta={meta} isBlogPost />
-      {children}
-    </article>
+    <>
+      <div id="#progress"></div>
+      <article className="flex flex-col justify-center max-w-2xl mx-auto mb-4 w-full">
+        <AuthorHeadPost meta={meta} isBlogPost />
+        {children}
+      </article>
+    </>
   );
 }
