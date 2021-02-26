ccfc4af53ffe49fb2c007e82865f8cd8d2ad8ba4
oliverloops <oliveralr@protonmail.com>
2021-02-26 00:41:44 -0600

    some bugs fixed

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -1,18 +1,8 @@
-import React from "react";
 import AuthorHeadPost from "./AuthorHeadPost";
 
 export default function BlogPost({ children, meta }) {
-  React.useEffect(() => {
-    let bar = document.querySelector("#progress");
-    window.addEventListener("scroll", () => {
-      let max = document.body.scrollHeight - innerHeight;
-      bar.style.width = `${(pageYOffset / max) * 100}%`;
-    });
-  }, []);
-
   return (
     <>
-      <div id="#progress"></div>
       <article className="flex flex-col justify-center max-w-2xl mx-auto mb-4 w-full">
         <AuthorHeadPost meta={meta} isBlogPost />
         {children}
