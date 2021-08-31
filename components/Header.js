560dee6942d2c3ff70c114eaa909b8d952f84d36
oliverloops <oliveralr@protonmail.com>
2021-08-31 17:21:33 -0500

    New text name logo added

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { motion } from "framer-motion";
 import { IoMdMoon } from "react-icons/io";
 import { HiSun } from "react-icons/hi";
@@ -31,21 +32,27 @@ export default function Header() {
       className="flex justify-between items-center mx-auto max-w-4xl w-full sticky sticky-nav
       h-18 p-8 py-8 bg-white dark:bg-black bg-opacity-60"
     >
-      <div className="pr-8">
-        <motion.button
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          onClick={() => switchTheme()}
-          className="flex justify-center items-center rounded-md bg-gray-200 dark:bg-gray-700 h-10 w-10"
-        >
+      <Link href="/">
+        <a>
           {darkContext.theme === "dark" ? (
-            <HiSun className="icon dark:text-white" />
+            <Image
+              src={"/oliverloops_white.svg"}
+              width={150}
+              height={80}
+              alt={"oliverloops"}
+            />
           ) : (
-            <IoMdMoon className="icon" />
+            <Image
+              src={"/oliverloops_black.svg"}
+              width={150}
+              height={80}
+              alt={"oliverloops"}
+            />
           )}
-        </motion.button>
-      </div>
-      <div className="flex justify-evenly flex-row w-42 px-8 pr-2">
+        </a>
+      </Link>
+
+      <div className="flex justify-evenly items-center w-42 px-8 pr-2">
         <div className="p-1 sm:p-4">
           <Link href="/portfolio">
             <a className="text-md dark:text-white">Portfolio</a>
@@ -61,10 +68,19 @@ export default function Header() {
             <a className="text-md dark:text-white">About</a>
           </Link>
         </div>
-        <div className="p-1 sm:p-4">
-          <Link href="/">
-            <a className="text-md dark:text-white">Home</a>
-          </Link>
+        <div className="p-4 sm:p-4">
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            onClick={() => switchTheme()}
+            className="flex justify-center items-center rounded-md bg-gray-200 dark:bg-gray-700 h-10 w-10"
+          >
+            {darkContext.theme === "dark" ? (
+              <HiSun className="icon dark:text-white" />
+            ) : (
+              <IoMdMoon className="icon" />
+            )}
+          </motion.button>
         </div>
       </div>
     </motion.nav>
