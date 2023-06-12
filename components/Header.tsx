a586bd1a77c732a18fb07a163078b4851a93a0c7
oliverloops <oliveralr@protonmail.com>
2023-06-11 23:52:12 -0600

    Header design update

--- components/Header.tsx
+++ components/Header.tsx
@@ -47,9 +47,9 @@ export default function Header() {
   };
 
   return (
-    <>
-      <div className="transition duration-300 flex justify-center items-center px-8 py-4 md:px-0 md:py-0 bg-gray-50 dark:bg-gray-900 bg-opacity-40 dark:bg-opacity-60 sticky sticky-nav">
-        <nav className="flex flex-col md:flex-row justify-between w-full relative max-w-3xl mx-auto md:py-2 md:h-18 ">
+    <div className="flex justify-center parent-sticky-nav">
+      <div className="transition duration-300 flex justify-center items-center w-full md:w-3/4 xl:w-1/2 px-8 py-4 md:px-0 md:py-0 mt-4 bg-gray-200 rounded-full border-slate-200 dark:bg-gray-700 bg-opacity-40 dark:bg-opacity-60 sticky sticky-nav">
+        <nav className="flex flex-col md:flex-row justify-between items-center w-full relative max-w-3xl mx-4 md:py-2 md:h-16">
           <div className="flex flex-col justify-center">
             <Link href="/">
               <a className="oliverloops text-xl md:text-2xl dark:text-white">
@@ -182,6 +182,6 @@ export default function Header() {
           </div>
         </li>
       </ul>
-    </>
+    </div>
   );
 }
